8ccb82c740b7aa444a2856e9e1f4890e3f6e1615
Matt Kong <mattkong@umich.edu>
2019-04-01 18:36:40 -0400

    Remove react from babelrc as it was causing issues. Add first react components.

---
+++ mattkong/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+	render() {
+		return (
+			<p>Things</p>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
