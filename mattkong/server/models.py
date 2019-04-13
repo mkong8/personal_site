5a3c14865d7ab9a89b8cbbc27b6606fe6edfb94d
Matt Kong <mattkong@umich.edu>
2019-04-12 23:25:42 -0400

    Add static image icons to socialmedia table.

--- mattkong/server/models.py
+++ mattkong/server/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class SocialMedia(models.Model):
 	name = models.CharField(max_length=100)
 	link = models.CharField(max_length=100)
+	dark_image = models.CharField(max_length=100)
+	light_image = models.CharField(max_length=100)
